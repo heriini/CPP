5d88836bdf77a1a24ff615d078753632895412f3
heriiini <iniherii@gmail.com>
2025-01-19 23:27:26 +0700

    sintaks for aritmatic operator

--- 07_aritmatika/main.cpp
+++ 07_aritmatika/main.cpp
@@ -1,8 +1,29 @@
+/* Operator Aritmatika 
+    * Penjumlahan (+)
+    * Pengurangan (-)
+    * Perkalian (*)
+    * Pembagian (/)
+    * Modulus / sisa hasil bagi (%)
+*/
+
 #include <iostream>
 
+    int a = 10;
+    int b = 2;
+
 int main(){
-    std::cout << "Test" << std::endl;
-    std::cout << "test lagi" << std::endl;
-    std::cout << "Sekali lagi" << std::endl;
+
+    int penjumlahan = a + b;
+    int pengurangan = a - b;
+    int perkalian = a * b;
+    int pembagian = a / b;
+    int mod = a % b;
+
+    std::cout << "Hasil penjumlahan dari a + b = " << penjumlahan << std::endl;
+    std::cout << "Hasil pengurangan dari a - b = " << pengurangan << std::endl;
+    std::cout << "Hasil perkalian dari a * b = " << perkalian << std::endl;
+    std::cout << "Hasil pembagian dari a / b = " << pembagian << std::endl;
+    std::cout << "Hasil mod dari a % b = " << mod << std::endl;
+
     return 0;
 }
