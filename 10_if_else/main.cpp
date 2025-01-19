b854c13528289a20f23af41e68d37679d2fdb902
heriiini <iniherii@gmail.com>
2025-01-20 01:12:08 +0700

    change the sintaks

--- 10_if_else/main.cpp
+++ 10_if_else/main.cpp
@@ -9,10 +9,16 @@ int main(){
     // jika variabel a bernilai 1 maka akan menjalankan fungsi oini
     if(a == 1){
         std::cout << "kamu mengetikan angka 1" << std::endl;
-    
+    } 
+
+     // lain jika variabel a bernilai 5 maka akan menjalankan fungsi ini
+    else if(a == 5){
+        std::cout << "kamu mengetikan angka 5" << std::endl;
+    }
+
     // variabel a tidak bernilai 1 maka jalankan fungsi ini
-    }else {
-        std::cout << "pasti kamu mengetik bukan angka 1" << std::endl;
+    else {
+        std::cout << "pasti kamu mengetik bukan angka 1 maupun 5" << std::endl;
     }
 
     std::cout << "oke...!!!" << std::endl;
