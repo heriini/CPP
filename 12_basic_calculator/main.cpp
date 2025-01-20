b85909dde2b2289b77ef88779e8e84e05bea66ee
heriiini <iniherii@gmail.com>
2025-01-20 10:26:27 +0700

    add folder 12_basic_calculator

--- 12_basic_calculator/main.cpp
+++ 12_basic_calculator/main.cpp
@@ -1,10 +1,74 @@
 #include <iostream>
 
-int main(){
-    float a, b;
+    // Varible Global 
+    float a, b, hasil;
     char aritmatika;
 
-    std::cout << "Basic Calculator Program" << std::endl;
+// Fungsi menjalankan program menggunakan if_statment
+void if_statment(void){
+    if(aritmatika == '+'){
+        hasil = a + b;
+        std::cout << " = " << hasil << "\n\n";
+    } 
+    else if(aritmatika == '-'){
+        hasil = a - b;
+        std::cout << " = " << hasil << "\n\n";
+    }
+    else if(aritmatika == '*'){
+        hasil = a * b;
+        std::cout << " = " << hasil << "\n\n";
+    }
+    else if(aritmatika == '/'){
+        hasil = a / b;
+        std::cout << " = " << hasil << "\n\n";
+    } 
+    else {
+        std::cout << "\nOperator yang anda masukan salah...!!!\n\n";
+    }
+}
+
+// Fungsi menjalankan program menggunakan switch_case
+void switch_case(void){
+    switch (aritmatika)
+    {
+    case '+':
+        hasil = a + b;
+        std::cout << " = " << hasil << "\n\n";
+        break;
+    case '-':
+        hasil = a - b;
+        std::cout << " = " << hasil << "\n\n";
+        break;
+    case '*':
+        hasil = a * b;
+        std::cout << " = " << hasil << "\n\n";
+        break;
+    case '/':
+        hasil = a / b;
+        std::cout << " = " << hasil << "\n\n";
+        break;
+    default:
+        std::cout << "\nOperator yang anda masukan salah...!!!\n\n";
+        break;
+    }
+}
+
+int main(){
+
+    std::cout << "*** Basic Calculator Program ***\n\n";
+
+    std::cout << "Masukan nilai pertama : ";
+    std::cin >> a;
+    std::cout << "Pilih operator (+), (-), (*), (/) : ";
+    std::cin >> aritmatika;
+    std::cout << "Masukan nilai kedua : ";
+    std::cin >> b;
+
+    std::cout << "Hasil perhitungan : ";
+    std::cout << a << " " <<  aritmatika << " " << b;
+
+    if_statment();      // Menggunakan if_statment
+    //switch_case();      // Menggunakan switch_case
 
     return 0;
 }
