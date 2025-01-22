7e0d7c8c72780686e82ffc7893850b2907b5b819
heriiini <iniherii@gmail.com>
2025-01-22 10:58:55 +0700

    update sintaks

--- 19_bentuk/main.cpp
+++ 19_bentuk/main.cpp
@@ -8,6 +8,7 @@ int main(){
     std::cin >> n;
     std::cout << std::endl;
 
+    std::cout << "Segitiga siku-siku" << std::endl;
     std::cout << "Pola 1 : " << std::endl << std::endl;
     for(int i = 1; i <= n; i++){
         for(int j = 1; j <= i; j++){
@@ -44,5 +45,49 @@ int main(){
         }
         std::cout << std::endl;
     }
+
+    std::cout << "\nSegitiga sama kaki" << std::endl;
+    std::cout << "Pola 1 : " << std::endl << std::endl;
+    for(int i = 1; i <= n; i++){
+        for(int j = n; j >= i; j--){
+            std::cout << " ";
+        }
+        for(int k = 1; k <= (2*i - 1); k++){
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "\nPola 2 : " << std::endl << std::endl;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            std::cout << " ";
+        }
+        for(int k = n; k >= (2*i - n); k--){
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "\nBelah ketupat :" << std::endl;
+    for(int i = 1; i <= n; i++){
+        for(int j = n; j >= i; j--){
+            std::cout << " ";
+        }
+        for(int k = 1; k <= (2*i - 1); k++){
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+    for(int i = 2; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            std::cout << " ";
+        }
+        for(int k = n; k >= (2*i - n); k--){
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+    
     return 0;
 }
