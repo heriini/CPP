7140191a7325b9a7e7816d4c6015666fa4fb8b92
heriiini <iniherii@gmail.com>
2025-01-22 11:44:51 +0700

    add folder 20_function

---
+++ 20_function/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+
+    int x;
+
+    std::cout << "masukan nilai x: ";
+    std::cin >> x;
+
+    std::cout << "akar kuadrat dari " << x << " adalah : " << sqrt(x) << std::endl;
+
+    return 0;
+}
