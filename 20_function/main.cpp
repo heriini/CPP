37a4d3b7e03410f7f88d78e64b5ae70249da22a6
heriiini <iniherii@gmail.com>
2025-01-23 11:49:25 +0700

    change sintaks

--- 20_function/main.cpp
+++ 20_function/main.cpp
@@ -1,14 +1,72 @@
+/*** Function / fungsi
+ * 
+ * ada yang dengan kembalian dan ada pula yang tidak
+ * 
+ */
+
 #include <iostream>
-#include <cmath>
+
+// fungsi dengan kembalian (return)
+int tambah (int a, int b){
+    int c;
+    c = a + b;
+    return c;
+}
+
+int kurang (int a, int b){
+    int c;
+    c = a - b;
+    return c;
+}
+
+int kali(int a, int b){
+    int c;
+    c = a * b;
+    return c;
+}
+
+float bagi(float a, float b){
+    float c;
+    c = a / b;
+    return c;
+}
 
 int main(){
 
-    int x;
+    int a, b, hasil, hasil1, hasil2;
+    float a2, b2, hasil3;
 
-    std::cout << "masukan nilai x: ";
-    std::cin >> x;
+    std::cout << "Penjumlahan" << std::endl;
+    std::cout << "masukan nilai a: ";
+    std::cin >> a;
+    std::cout << "masukan nilai b: ";
+    std::cin >> b;
+    hasil = tambah(a,b);
+    std::cout << "Hasil dari  " << a << " + " << b << " adalah : " << hasil << std::endl;
 
-    std::cout << "akar kuadrat dari " << x << " adalah : " << sqrt(x) << std::endl;
+    std::cout << "\nPengurangan" << std::endl;
+    std::cout << "masukan nilai a: ";
+    std::cin >> a;
+    std::cout << "masukan nilai b: ";
+    std::cin >> b;
+    hasil1 = kurang(a,b);
+    std::cout << "Hasil dari  " << a << " - " << b << " adalah : " << hasil1 << std::endl;
+
+    std::cout << "\nPerkalian" << std::endl;
+    std::cout << "masukan nilai a: ";
+    std::cin >> a;
+    std::cout << "masukan nilai b: ";
+    std::cin >> b;
+    hasil2 = kali(a,b);
+    std::cout << "Hasil dari  " << a << " * " << b << " adalah : " << hasil2 << std::endl;
+
+    std::cout << "\nPembagian" << std::endl;
+    std::cout << "masukan nilai a: ";
+    std::cin >> a2;
+    std::cout << "masukan nilai b: ";
+    std::cin >> b2;
+    hasil3 = bagi(a2, b2);
+    std::cout << "Hasil dari  " << a << " / " << b << " adalah : " << hasil3 << std::endl;
 
     return 0;
 }
