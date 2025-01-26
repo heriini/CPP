bf6b906235ac5ffa45a21dad2ba3a7ee7afc6e29
heriiini <iniherii@gmail.com>
2025-01-26 17:44:10 +0700

    add folder 28_looping_array

---
+++ 28_looping_array/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(){
+
+    int nilai[10] = {1,2,3,4,5,6,7,8,9,10};
+
+    for(int i : nilai){
+        std::cout << i << " alamatnya   : " << &nilai << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    for(int &iref : nilai){
+        iref *= 2;
+
+        std::cout << iref << " alamatnya   : " << &iref << std::endl;
+    }
+    return 0;
+}
