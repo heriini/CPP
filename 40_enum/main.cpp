f7d6735f023a3ec257d4984ce57fbf6518838be7
heriiini <iniherii@gmail.com>
2025-02-11 18:15:01 +0700

    add folder 39 & 40

---
+++ 40_enum/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+enum warna {merah, kuning, hijau, biru, ungu, coklat, hitam, putih};
+
+int main(){
+    
+    warna baju;
+
+    baju = hijau;
+
+    std::cout << baju << std::endl;
+
+    return 0;
+}
